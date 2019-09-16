c7c0bc04a7abc49498f98c6c916c9bdc63f23837
Kruchy1980 <jakub.kruszynski@vp.pl>
2019-09-16 21:55:42 +0200

    Console logging Edited

---
+++ console-logging/scripts/console_log.js
@@ -0,0 +1,5 @@
+// Firstly we need to declare variable
+var myName = "Jakub";
+// Now we can insert in document our variable by command
+document.write(myName); // document is an object and "write" is a method.
+// We can also enter numbers and string, math operators ana any math actions as well not only variables.
