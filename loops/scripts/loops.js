731c0261905d28c66a875595558c07fc71551bc5
Kruchy1980 <jakub.kruszynski@vp.pl>
2019-09-18 15:52:02 +0200

    Loops finalized :)

--- loops/scripts/loops.js
+++ loops/scripts/loops.js
@@ -7,7 +7,8 @@ while (age < 10) { // our condirion so while age will be below 10
     
 }
 // This will show to document when the count come to 10.
-document.write("You are now over 10");
+console.log("You are now over 10");
+
 
 // For Loops:
 // As we can see first parameter of for loop is variable(no need to write it separately but the name can not be complex - in general the "i is ussed for that.")
@@ -18,7 +19,8 @@ for (var i = 175; i <= 200; i++) {
     console.log("Your Height is " + i + " cm");
 }
 // And this will happen when the condition will be met
-alert("You are finally 2m hight!");
+console.log("You are finally 2m hight!");
+
 
 // Do while loop
 // Remember to set what is going to be done
@@ -29,3 +31,25 @@ do {
 }
 // Now we create the condition
 while (speed  > 100);
+
+// Using break keyword
+for (var i = 175; i <= 200; i++) {
+    if (i == 180 || i == 185) {
+        continue; // continue will continuing the loop without printing the values which wwe set under the conditions. 
+    }
+    console.log(i);
+
+    if(i == 190) {
+        break; // break will totally finish our loop
+    }
+}
+// And this will happen when the brake works.
+console.log("Your current height is " + i + " cm. You have to grow couple more centimeters." );
+
+// Practical esample of using JS Loops.
+// Firstly we need to mark an element we want to make action with
+var links = document.getElementsByTagName("a");
+// Now we can prepare loop
+for (i = 0; i < links.length; i++) {
+    links[i].className = "link - " + i;
+}
