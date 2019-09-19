9677a1897de342f3b042974e3957353235f4f658
Kruchy1980 <jakub.kruszynski@vp.pl>
2019-09-19 19:05:28 +0200

    Repository finished but not fully merged yet

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
  Prework-iSA-JavaScript
 Some code of JS - will be fulfilled later.
+Some infos about JS libraries:
+Libraries are vey needed sources of tools for every programming language:
+    1. jQuery -  https://code.jquery.com/ - one of the most helpful library for JS.
+Other helpfull tools for JS:
+    MooTools
+    Modernizr
+    AJAX
+    Json with Ajax
+Other Staff:
+    Regular Expresions
+    Advanced Validations
+    OOP's
+    Sliders, other animators
+
+The information in this file are only for generat information purposes for interested in the JS Beginners;)
+
---
+++ forms/scripts/forms.js
@@ -0,0 +1,31 @@
+// There is easy method to get an elements from form
+// how we can set the form we want:
+document.forms.myForm; //- it is enough to get know that we eant elements from form.
+// Now we can create the variable of form to have better acces
+var myInfo = document.forms.myForm;
+// Now we cn take any form element we want.
+myInfo.name; // - it shows us a inforamation from input name.
+// Now any of the elements we can get by name of the element
+myInfo.colour; // etc.
+// now we can use onfocus event eg. it works when some part is marked
+myInfo.name.onfocus = function() {
+    myInfo.name.style.border = "4px dashed orange";
+}
+// onblur when element is not focused its happen.
+myInfo.name.onblur = function() {
+    myInfo.name.style.border = "none";
+}
+//------------------------------------------------------
+//How to validate forms. onSubmit - first variable the simplest validation that can be.
+var messeageonsub = document.getElementById("messonsub");
+
+myInfo.onsubmit = function() {
+    if(myInfo.name.value == "") {
+        messeageonsub.innerHTML = "Please Enter the name";
+        return false;
+    } else {
+        messeageonsub.innerHTML = "";
+        return true;
+    }
+}
+
